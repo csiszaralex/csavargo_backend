05aaec63505079db131fc3b2c475b2087ce9f88a
csiszaralex <csiszaralex@gmail.com>
2022-08-18 16:43:50 +0200

    :wrench: Adds .env file for config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const logger = new Logger('START');
-
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('START');
+  const configService = app.get(ConfigService);
 
-  await app.listen(process.env.PORT || 3000, () => {
+  await app.listen(configService.get('PORT') || 3000, () => {
     logger.debug('-----------------------');
   });
 }
+
 bootstrap();
