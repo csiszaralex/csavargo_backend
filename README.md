2bb5d3198296b4b70ed09e4f8fea6f67448dc340
csiszaralex <csiszaralex@gmail.com>
2022-08-18 16:28:17 +0200

    :tada: Start project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Csavargó Backend
---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger('START');
+
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(process.env.PORT || 3000, () => {
+    logger.debug('-----------------------');
+  });
+}
+bootstrap();
